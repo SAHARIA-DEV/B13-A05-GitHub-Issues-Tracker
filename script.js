24e333c43bf71a26b4009a3ad1929e1e64faf6d0
sahariaoex2022 <sahariaoex2022@gmail.com>
2026-03-07 03:42:06 +0600

    Create main page UI

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+const API = "https://phi-lab-server.vercel.app/api/v1/lab/issues";
+
+const container = document.getElementById("issuesContainer");
+const spinner = document.getElementById("spinner");
+
 function login(e) {
 
     e.preventDefault();
@@ -6,14 +11,11 @@ function login(e) {
     const pass = document.getElementById("password").value;
 
     if (user === "admin" && pass === "admin123") {
-
         window.location = "main-page.html";
-
     }
     else {
-
         alert("Invalid login");
-
     }
 
 }
+
