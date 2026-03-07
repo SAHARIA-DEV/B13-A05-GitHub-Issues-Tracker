4f9ebf469f90b76b6f39526c68ce67a003415f80
sahariaoex2022 <sahariaoex2022@gmail.com>
2026-03-08 00:40:03 +0600

    updated modal page

--- script.js
+++ script.js
@@ -95,11 +95,11 @@ ${issue.priority}
 
 </div>
 
-<h3 class="font-semibold mt-2">
+<h3 class="font-semibold mt-2 text-[16px]">
 ${issue.title}
 </h3>
 
-<p class="text-sm text-gray-500 mt-1">
+<p class="text-[14px] text-gray-500 mt-1">
 ${issue.description}
 </p>
 
@@ -109,17 +109,17 @@ ${issue.description}
 BUG
 </span>
 
-<span class="bg-yellow-100 text-yellow-600 px-2 py-1 rounded">
+<span class="bg-yellow-100 text-[12px] text-yellow-600 px-2 py-1 rounded">
 HELP WANTED
 </span>
 
 </div>
 
-<div class="text-xs text-gray-500 mt-3">
+<div class="text-[14px] text-gray-500 mt-3">
 #${issue.id} by ${issue.author}
 </div>
 
-<div class="text-xs text-gray-400">
+<div class="text-[14px] text-gray-400">
 ${issue.createdAt}
 </div>
 
